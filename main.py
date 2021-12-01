5d8b7d6cccdc9efa9f6ce6ab0e384e88c26c1e9d
dl1364 <94717418+dl1364@users.noreply.github.com>
2021-12-01 15:22:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,99 @@
 import string
 import login
+
+def menu():
+    while True:
+        print("Method&Tools 1 Shopping Menu")
+        print("\t")
+        print("1. Search for item")
+        print("2. Cart")
+        print("3. Order History")
+        print("4. Account Settings")
+        print("\t")
+        print("What would you like to do?")
+        option = input("Command: ")
+        print("\t")
+
+        if option == "1":
+            while True:
+                print("1. Books")
+                print("2. Movies")
+                print("\t")
+                input("Which items are you searching for: ")
+                print("\t")
+                if option == "1":
+                    while True:
+                        print("1. Search by Genre")
+                        print("2. Search by Name")
+                        print("3. Display all Books")
+                        print("4. Go Back")
+                        input("What would you like to do: ")
+                        print("\t")
+                        if option == "1":
+                            print("1")
+                        elif option == "2":
+                            print("2")
+                        elif option == "3":
+                            print("3")
+                        elif option == "4":
+                            print("4")
+                if option == "2":
+                    while True:
+                        print("1. Search by Genre")
+                        print("2. Search by Name")
+                        print("3. Display all Movies")
+                        print("4. Go Back")
+                        input("What would you like to do: ")
+                        print("\t")
+                        if option == "1":
+                            print("1")
+                        elif option == "2":
+                            print("2")
+                        elif option == "3":
+                            print("3")
+                        elif option == "4":
+                            print("4")
+        if option == "2":
+            while True:
+                print("1. Add an item from cart")
+                print("2. Remove an item from cart")
+                print("3. Clear entire cart")
+                print("4. Checkout")
+                print("5. Go back")
+                print("\t")
+                input("What would you like you with your cart?")
+                print("\t")
+                if option == "1":
+                    print("1")
+                elif option == "2":
+                    print("2")
+                elif option == "3":
+                    print("3")
+                elif option == "4":
+                    print("4")
+                elif option == "5":
+                    print("5")
+        if option == "3":
+            while True:
+                print("Order History: ")
+                print("\t")
+        if option == "4":
+            while True:
+                print("1. Update Information")
+                print("2. Delete Account")
+                print("3. Go Back")
+                print("4. Logout")
+                print("\t")
+                input("What would you like to do with your account?")
+                print("\t")
+                if option == "1":
+                    print("1")
+                elif option == "2":
+                    print("2")
+                elif option == "3":
+                    print("3")
+                elif option == "4":
+                    print("4")
 while True:
     print("Welcome to Method&Tools Group 1 Shopping!")
     print("\t")
