49fbfbfe738c9a9825b8023c80f6a37878c68b54
dl1364 <94717418+dl1364@users.noreply.github.com>
2021-12-01 18:20:38 -0600

    Update book.py

--- book.py
+++ book.py
@@ -12,3 +12,17 @@ class Book:
             currItemArray = currItem.split("|")
             print(currItemArray[0] + "\n")
             i += 1
+        
+        option = input("Which book would you like to look more at: ")
+        #compare to file
+                file1 = open('classBooks.txt', 'r')
+                Items = file1.readlines()
+                i = 0
+                exists = 0
+                for j in Items:
+                    currItem = Items[i]
+                    currItemArray = currItem.split("|")
+                    if currItemArray[0] == option:
+                        exists = 1
+                    i += 1
+                file1.close()
