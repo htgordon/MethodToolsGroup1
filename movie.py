4489a43a27bb81c5c156589a1009e3eded26788e
dl1364 <94717418+dl1364@users.noreply.github.com>
2021-12-01 19:49:52 -0600

    Create movie.py

---
+++ movie.py
@@ -0,0 +1,45 @@
+import activeUser
+
+class Movie:
+  def displayAll(self):
+    while True:
+        # readlines()
+        file1 = open('classMovies.txt', 'r')
+        Items = file1.readlines()
+
+        #     s to command line, not needed for final
+        #Read first element from each item
+        i = 0
+        for j in Items:
+            currItem = Items[i]
+            currItemArray = currItem.split("|")
+            print(currItemArray[0] + "\n")
+            i += 1
+        
+        option = input("Which book would you like to look more at: ")
+        #compare to file
+        file1 = open('classMovies.txt', 'r')
+        Items = file1.readlines()
+        i = 0
+        exists = 0
+        index = -1
+        for j in Items:
+            currItem = Items[i]
+            currItemArray = currItem.split("|")
+            if currItemArray[0] == option:
+                exists = 1
+                index = i
+            i += 1
+        file1.close()
+        if exists != 1:
+                ("Book does not exist\n")
+        else:
+                print(Items[index])
+                print("What would you like to do next: ")
+                print("1. Add book to cart")
+                print("2. Go Back")
+                option = input(": ")
+                if option == "1":
+                    print("1")
+                if option == "2":
+                    break
